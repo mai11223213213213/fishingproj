791732a707be62f402b3d8228dc7bc6cc1610cea
mai11223213213213 <matvijko1606@gmail.com>
2025-03-25 23:41:06 +0200

    sasasas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CatchFish from './components/CatchFish';
 
 function App() {
   return (
     <div className="App">
-      
+      <CatchFish/>
     </div>
   );
 }
--- src/components/CatchFish.tsx
+++ src/components/CatchFish.tsx
@@ -1,16 +1,96 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import { preconnect } from 'react-dom';
+
 const generateRandomNumber = (min: number, max: number): number => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
 const CatchFish = () => {
-    let [fishPos, setFishPos]  = useState(generateRandomNumber(1,500))
+    const [fishPos, setFishPos] = useState(generateRandomNumber(1, 500));
+    const [isMouseDown, setIsMouseDown] = useState(true);
     let [floatPos, setFloatPos] = useState(fishPos)
+    let [newFishPos, setNewFishPos] = useState(generateRandomNumber(1, 500))
+    let [fishSpeed, setFishSpeed] = useState(generateRandomNumber(1, 2))
+    
+    useEffect(()=>{
+        
+        const interval = setInterval(() => {
+            setFishPos( (prevPos : any) => {
+                if(prevPos != 500 ){
+                    if(prevPos !=0){
+                        if(prevPos > newFishPos){
+                            return prevPos - fishSpeed*3
+
+                        }
+                        else if(prevPos < newFishPos){
+                            return prevPos + fishSpeed*3
 
 
+                        }
+                        else if(prevPos == newFishPos){
+                            return prevPos -1 
+                        }
+
+                    }
+                }
+
+
+            });
+        }, 100);
+        setNewFishPos(generateRandomNumber(1,500))
+        setFishSpeed(generateRandomNumber(1,4))
+        
+        return () => clearInterval(interval);
+
+
+    }, [fishPos])
+
+    useEffect(() => {
+       
+        const handleMouseDown = (event: MouseEvent) => {
+            if (event.button === 0) { // Ліва кнопка миші
+                setIsMouseDown(false);
+            }
+        };
+        const handleMouseUp = (event: MouseEvent) => {
+            if (event.button === 0) {
+                setIsMouseDown(true);
+            }
+        };
+                
+        document.addEventListener('mousedown', handleMouseDown);
+        document.addEventListener('mouseup', handleMouseUp);
+
+        return () => {
+            document.removeEventListener('mousedown', handleMouseDown);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFloatPos(prevPos => isMouseDown ? prevPos + 3 : prevPos - 3);
+        }, 10);
+        
+        return () => clearInterval(interval);
+    }, [isMouseDown]);
 
     return (
-        <div>CatchFish</div>
-    )
-}
+        <div className="scale">
+            <div 
+                className="float" 
+                style={{ position: "absolute", backgroundColor: "purple", width: "200px", height: "200px", left: "20px", top: floatPos }}
+            ></div>
+            <div 
+                className="fish"
+                style={{ position: "absolute", backgroundColor: "red", width: "100px", height: "100px", left: "20px", top: fishPos, zIndex:'1', margin:'0 auto'}}
 
-export default CatchFish
+            >
+
+            </div>
+
+        </div>
+    );
+};
+
+export default CatchFish;
