8417b0b5ee872b9f6303598c60588dba3d3577f6
mai11223213213213 <matvijko1606@gmail.com>
2025-04-03 17:51:44 +0300

    qfadfafasfsfsfsdfsfsdf

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -12,13 +12,14 @@ const Inventory = ({invContent}:inv) => {
   return (
     <div className="inv-box">{invContent.map((el) => (
         <div className="fish-box">
+            <p className="fish-name">{el.name}</p>
             <img src={el.imgSrc} alt="" className="fish-img" />
             <p className="fish-quality">Quality:{el.quality}</p>
             <div className="info"> 
                 <p className="fish-chance">Chance:{String(el.chance)}</p>
                 <p className="fish-price">Price:{String(el.price)}</p>
             </div>
-            <Button width='30%' height='20%' onClickE={sold}>SOLD</Button>
+            <Button width='55%' height='20%' onClickE={sold}>SOLD</Button>
 
 
 
