44ccfcd1565510afffbdd5ce027bd6d59271d3a7
mai11223213213213 <matvijko1606@gmail.com>
2025-04-29 20:52:25 +0300

    jjkkjjkkj

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -12,7 +12,7 @@ interface inv{
 
 const Inventory = ({invContent, invUpdate, setBalance}:inv) => {
     const sold = (event: React.MouseEvent<HTMLButtonElement>) =>{
-            const btns = document.querySelectorAll(".btn-sold")
+            const btns = document.querySelectorAll(".btn-sell")
             const btn = event.currentTarget
             
             console.log(btns)
@@ -47,7 +47,7 @@ const Inventory = ({invContent, invUpdate, setBalance}:inv) => {
                 <p className="fish-chance">Chance:{String(el.chance)}</p>
                 <p className="fish-price">Price:{String(el.price)}</p>
             </div>
-            <Button width='64%' height='20%' onClickE={sold}>SOLD</Button>
+            <Button width='64%' height='20%' onClickE={sold}>SELL</Button>
 
 
         </div>
